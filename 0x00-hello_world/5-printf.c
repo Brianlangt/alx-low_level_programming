89a1a1d5015ad3f826b9b4407745c82f0e1e9650
Brianlangt <brianlangt@gmail.com>
2022-09-09 13:12:56 -0700

    C Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,with proper 
+	printf("with proper grammar, but the outcome is a piece of art,with proper
 grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
