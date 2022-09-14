4286fb0fd74d25ac10e9d11ca5661f8ff1ce3856
Brianlangt <brianlangt@gmail.com>
2022-09-14 16:07:43 -0700

    Commit Update

---
+++ coding2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int max(int num1, int num2) 
+	{
+		
+		int result;
+
+   		if (num1 > num2)
+      			result = num1;
+   		else
+      			result = num2;
+
+   		return result;
+	}
+
+	return (0);
+}
