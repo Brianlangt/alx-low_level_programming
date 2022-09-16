4ca2b73e2b64959afaa9e3989111941f79fe7aa1
Brianlangt <brianlangt@gmail.com>
2022-09-15 17:12:29 -0700

    Commit Update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * printalphabet - printts the alphabet in lower case 
+ * 
+ * Return= always 0
+ */
+
+void printalphabet()
+{
+	int i
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\');
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printfirstone();
+	printalphabet();
 
 	return (0);
 
