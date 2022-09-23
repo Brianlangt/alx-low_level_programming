0101aca9065073b062ab0e3be2a64688be563cea
Brianlangt <brianlangt@gmail.com>
2022-09-23 09:58:51 -0700

    Updates Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
 * main - program to print whether something is positive or negative
 *
-* Return = 0
+* Return= Always 0
 */
 
 int main(void)
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@ include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always
  */
 int main(void)
 {
