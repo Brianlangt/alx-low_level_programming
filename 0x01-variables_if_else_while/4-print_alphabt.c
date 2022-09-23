8beac3e9df939ca3473f6ddfc20c374daddaa681
Brianlangt <brianlangt@gmail.com>
2022-09-23 14:30:18 -0700

    Updates Variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,18 @@ int main(void)
 
 	for (ch = 'a'; ch < 'z'; ++ch)
 	{
-		if (ch == 'q'; ch == 'e')
-		continue;
-		putchar(ch);
+		if (ch == 'q')
+		{
+			continue;
+		}
+		else if (ch == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(ch);
+		}
 		putchar('\n');
 	}
 	return (0);
